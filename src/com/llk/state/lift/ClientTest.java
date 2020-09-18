f4e5e31d3ceb0616b11a3d854b1f0b2350e63402
liliankun <li_liankun@163.com>
2020-09-18 13:57:38 +0800

    no message

--- src/com/llk/state/lift/ClientTest.java
+++ src/com/llk/state/lift/ClientTest.java
@@ -4,7 +4,7 @@ public class ClientTest {
 
     public static void main(String[] args) {
         Context context = new Context();//创建上下文对象
-        context.openDoor();//打开电梯门，用进入电梯
+        context.openDoor();//打开电梯门，用户进入电梯
         context.closeDoor();//用户关闭电梯门
         context.run();//电梯开始运行
         context.chooseFloor();//选择楼层
@@ -13,7 +13,7 @@ public class ClientTest {
         context.openDoor();//电梯开门
 
         /*Context context = new Context();//创建上下文对象
-        context.openDoor();//打开电梯门，用进入电梯
+        context.openDoor();//打开电梯门，用户进入电梯
         context.closeDoor();//用户关闭电梯门
         context.run();//电梯开始运行
         context.chooseFloor();//选择楼层
